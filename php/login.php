d9f0b67eb0380c75045f58e173492fe6858ffb2b
Lorenzo.T <59581743+Lorenzo-Tr@users.noreply.github.com>
2022-03-29 23:22:57 +0200

    try work

--- php/login.php
+++ php/login.php
@@ -18,32 +18,29 @@ $user = $db->get('user', [], ['username' => $user_login['username']]);
 
 session_start();
 if ($user) {
-	// Account exist
-	$encrypt = $db_encrypt->get('encrypt', [], ['user_id' => $user->id]);
-	$db->change_encrypt_key(hex2bin($encrypt->key), hex2bin($encrypt->iv));
-	$user_ip_encrypt = $db->encrypt_data($_SERVER['REMOTE_ADDR']);
-	$user_user_agent_encrypt = $db->encrypt_data($_SERVER['HTTP_USER_AGENT']);
-	$ip = $db->get('ip_address', [], ['ip' => $db->encrypt_data($_SERVER['REMOTE_ADDR'])]);
-	var_dump($ip);
-	if(!$ip)
-		$db->create('ip_address', ['user_id'=> $ip->user_id,'ip' => $user_ip_encrypt, 'user_agent' => $user_user_agent_encrypt]);
-
-
-	if (password_verify($user_login['password'], $user->password)) {
-		$db->update('ip_address', ['try' => '0'], ['ip' => $user_ip_encrypt]);
-		$_SESSION['username'] = $username;
-		header('Location: ../public/profile.php');
-	} else {
-		$db->update('ip_address', ['try' => '`try`+1'], ['ip' => $user_ip_encrypt]);
-//				$db->run("UPDATE `ip_address` SET `try`=`try`+1 WHERE ip=?", [$db->encrypt_data($_SERVER['REMOTE_ADDR'])]);
-		$_SESSION['login_error'] = "wrong_password";
-		header('Location: ../public/connection.php');
-	}
+    // Account exist
+    $encrypt = $db_encrypt->get('encrypt', [], ['user_id' => $user->id]);
+    $db->change_encrypt_key(hex2bin($encrypt->key), hex2bin($encrypt->iv));
+    $ip = $db->get('ip_address', [], ['ip' => $db->encrypt_data($_SERVER['REMOTE_ADDR'])]);
+    if($ip->try < 5){
+        if (password_verify($user_login['password'], $user->password)) {
+            $db->update('ip_address', ['try' => 0], ['ip' => $ip->ip]);
+            $_SESSION['username'] = $username;
+            header('Location: ../public/profile.php');
+        } else {
+            $db->run("INSERT INTO `ip_address` (`ip`, `user_agent`, `try`) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE try=try+1",[$db->encrypt_data($_SERVER['REMOTE_ADDR']), $db->encrypt_data($_SERVER['HTTP_USER_AGENT']), 1]);
+            $_SESSION['login_error'] = "wrong_password";
+            header('Location: ../public/connection.php');
+        }
+    }else{
+        // TODO: Check if now - update_at = 15min
+        $_SESSION['login_error'] = "blocked_account";
+        header('Location: ../public/connection.php');
+    }
 } else {
-	$db->replace('ip_address', ['ip' => $db->encrypt_data($_SERVER['REMOTE_ADDR']), 'user_agent' => $db->encrypt_data($_SERVER['HTTP_USER_AGENT'])]);
-	$db->run("UPDATE `ip_address` SET `try`=`try`+1 WHERE ip=?", [$db->encrypt_data($_SERVER['REMOTE_ADDR'])]);
-	$_SESSION['login_error'] = "no_account_found";
-	header('Location: ../public/connection.php');
+    $db->run("INSERT INTO `ip_address` (`ip`, `try`) VALUES(?, ?) ON DUPLICATE KEY UPDATE try=try+1",[$_SERVER['REMOTE_ADDR'], 1]);
+    $_SESSION['login_error'] = "no_account_found";
+    header('Location: ../public/connection.php');
 }
 unset($db, $db_encrypt);
 exit();
